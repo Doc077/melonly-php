bfc1b522017f3a396d2ecd44863540041fc325e9
Doc077 <dom.rajkowski@gmail.com>
2022-01-09 21:03:49 +0100

    Remove variables scaffolding from default view

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,7 @@
 use Melonly\Routing\Router;
 use Melonly\Http\Request;
 use Melonly\Http\Response;
-use Melonly\Validation\Validator;
-use App\Models\User;
 
 Router::get('/', function (Request $request, Response $response): void {
-    $user = User::where('id', '=', 1)->orWhere('age', '>', 10)->fetch();
-
-    $response->view('pages.home', [
-        'name' => $user->name
-    ]);
+    $response->view('pages.home');
 });
-
-// Router::post('/login', function (Request $request, Response $response): void {
-//     Validator::check([
-//         'username' => ['required', 'min:3', 'max:32']
-//     ]);
-// });
