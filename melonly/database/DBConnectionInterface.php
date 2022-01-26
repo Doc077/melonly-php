ed050822501daf41c3aaff8e42bb6f05918db68c
Dominik Rajkowski <60520436+Doc077@users.noreply.github.com>
2022-01-26 14:39:33 +0100

    Delete melonly/database directory

--- melonly/database/DBConnectionInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Melonly\Database;
-
-use PDO;
-
-interface DBConnectionInterface {
-    public function getConnection(): PDO;
-}
--- melonly/database/Record.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Melonly\Database;
-
-class Record {
-    //
-}
