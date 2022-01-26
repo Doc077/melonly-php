92c85fe91665203af01c99aa60b8a06c5c667f38
Dominik Rajkowski <60520436+Doc077@users.noreply.github.com>
2022-01-26 14:38:06 +0100

    Delete melonly/exceptions directory

--- melonly/exceptions/set_error_handler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Melonly\Exceptions;
-
-set_error_handler(function (int | string $code, string $message = 'Uncaught error', string $file = 'index.php', int $line = 0) {
-    $notice = new Notice($code, $message, $file, $line);
-
-    Handler::handle($notice);
-});
