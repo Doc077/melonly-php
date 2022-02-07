53373d5e931d8a7737fc4b890aa63ed51dc88009
Doc077 <dom.rajkowski@gmail.com>
2022-02-07 18:53:02 +0100

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
 const path = require('path')
 
 module.exports = {
-    entry: path.join(__dirname, 'frontend', 'react', 'index.js'),
+  entry: path.join(__dirname, 'frontend', 'react', 'index.js'),
 
-    mode: 'development',
+  mode: 'development',
 
-    output: {
-        path: path.resolve(__dirname, 'public', 'js'),
-        filename: 'index.js',
-    },
+  output: {
+    path: path.resolve(__dirname, 'public', 'js'),
+    filename: 'index.js',
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react',
-                        ],
-                    },
-                },
-            },
-            {
-                test: /\.s[ac]ss$/,
-                exclude: /node_modules/,
-                use: [
-                    'sass-loader',
-                ],
-            },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/,
+        exclude: /node_modules/,
+        use: [
+          'sass-loader',
         ],
-    },
+      },
+    ],
+  },
 }
