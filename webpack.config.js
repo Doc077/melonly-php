f39ea196ac2fd8cdb9af5036a72ffe3922c5e56c
Doc077 <dom.rajkowski@gmail.com>
2022-02-15 20:36:01 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,17 @@
 const path = require('path')
 
 module.exports = {
-   entry: path.join(__dirname, 'frontend', 'react', 'index.js'),
-
    mode: 'development',
 
-   output: {
-      path: path.resolve(__dirname, 'public', 'js'),
-      filename: 'index.js',
-   },
+   // entry: path.join(__dirname, 'frontend', 'react', 'index.js'),
+
+   // output: {
+   //    path: path.resolve(__dirname, 'public', 'js'),
+   //    filename: 'index.js',
+   // },
 
    module: {
       rules: [
-         {
-            test: /\.?js$/,
-            exclude: /node_modules/,
-            use: {
-               loader: 'babel-loader',
-               options: {
-                  presets: [
-                     '@babel/preset-env',
-                     '@babel/preset-react',
-                  ],
-               },
-            },
-         },
-         {
-            test: /\.vue$/,
-            loader: 'vue-loader',
-         },
          {
             test: /\.s[ac]ss$/,
             exclude: /node_modules/,
@@ -36,6 +19,23 @@ module.exports = {
                'sass-loader',
             ],
          },
+         // {
+         //    test: /\.?js$/,
+         //    exclude: /node_modules/,
+         //    use: {
+         //       loader: 'babel-loader',
+         //       options: {
+         //          presets: [
+         //             '@babel/preset-env',
+         //             '@babel/preset-react',
+         //          ],
+         //       },
+         //    },
+         // },
+         // {
+         //    test: /\.vue$/,
+         //    loader: 'vue-loader',
+         // },
       ],
    },
 }
