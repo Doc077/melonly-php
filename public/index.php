039fc95f2be662910f17a40f19bc32def3e65715
dominiq <60520436+Doc077@users.noreply.github.com>
2022-04-25 20:36:49 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,15 @@
 
 use Melonly\Bootstrap\Application;
 
+/**
+ * The Modern PHP Framework
+
+ * (c) 2021 - 2022 Dominik Rajkowski
+
+ * Documentation: https://github.com/Doc077/melonly-php/blob/7.x/README.md
+ * Repository: https://github.com/Doc077/melonly-php
+ */
+
 // Path to framework files and packages.
 // Change this path when moving to server from local environment.
 
@@ -10,16 +19,3 @@ const INCLUDE_PATH = '../';
 require __DIR__ . '/' . INCLUDE_PATH . '/vendor/autoload.php';
 
 Application::start();
-
-/*
-    █▀▄▀█ █▀▀ █   █▀▀█ █▀▀▄ █   █  █
-    █ █ █ █▀▀ █   █  █ █  █ █   █▄▄█
-    █   █ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀  ▀ ▀▀▀ ▄▄▄█
-
-    The Modern PHP Framework
-
-    (c) 2021 - 2022 Dominik Rajkowski
-
-    Documentation: https://github.com/Doc077/melonly-php/blob/7.x/README.md
-    Repository: https://github.com/Doc077/melonly-php
-*/
