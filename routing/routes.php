b8e62fe1df1ce7e89051172bc5d5d3fe70386528
dominiq <60520436+Doc077@users.noreply.github.com>
2022-05-01 19:21:39 +0200

    Update routes.php

--- routing/routes.php
+++ routing/routes.php
@@ -8,11 +8,3 @@ use Melonly\Routing\Facades\Route;
 Route::get('/', function (Request $request, Response $response): void {
     $response->view('pages.home');
 });
-
-/**
- * Example route with assigned controller.
- * 
- * Controllers are stored in src/Controllers.
- * Create a new controller with new:controller command.
- */
-Route::get('/example/{slug}', [Controllers\ExampleController::class, 'index']);
