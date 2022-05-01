cc0b8fe6a28ba9422578bd66926e1cb28eafb0dc
dominiq <60520436+Doc077@users.noreply.github.com>
2022-05-01 14:30:24 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-const path = require('path')
+const { join, resolve } = require('path')
 
 const framework = 'react'
 
 module.exports = {
-   mode: 'development',
+  mode: 'development',
 
-   entry: path.join(__dirname, 'frontend', framework, 'index.js'),
+  entry: join(__dirname, 'frontend', framework, 'index.js'),
 
-   output: {
-      path: path.resolve(__dirname, 'public', 'js'),
-      filename: 'index.js',
-   },
+  output: {
+    path: resolve(__dirname, 'public', 'js'),
+    filename: 'index.js',
+  },
 
-   module: {
-      rules: [
-         {
-            test: /\.s[ac]ss$/,
-            exclude: /node_modules/,
-            use: [
-               'sass-loader',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        exclude: /node_modules/,
+        use: [
+          'sass-loader',
+        ],
+      },
+
+      // React.js config
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
             ],
-         },
+          },
+        },
+      },
 
-         // React.js config
-         // {
-         //    test: /\.?js$/,
-         //    exclude: /node_modules/,
-         //    use: {
-         //       loader: 'babel-loader',
-         //       options: {
-         //          presets: [
-         //             '@babel/preset-env',
-         //             '@babel/preset-react',
-         //          ],
-         //       },
-         //    },
-         // },
-
-         // Vue.js config
-         // {
-         //    test: /\.vue$/,
-         //    exclude: /node_modules/,
-         //    loader: 'vue-loader',
-         // },
-      ],
-   },
+      // Vue.js config
+      // {
+      //   test: /\.vue$/,
+      //   exclude: /node_modules/,
+      //   loader: 'vue-loader',
+      // },
+    ],
+  },
 }
